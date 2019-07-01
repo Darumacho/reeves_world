d33f4c13d83f70133f19daa9994dc2bc9bfa5b3b
Adrien L <a.lepinay@attineos.com>
2019-07-01 10:19:15 +0200

    Rajout react table

--- src/components/personnages/Amis.js
+++ src/components/personnages/Amis.js
@@ -45,7 +45,14 @@ export class Ami extends React.Component {
               <p><u>Fonction :</u> Directeur de la Tour de Combat</p>
               <p><u>Lieu(x) de rencontre :</u> Tour de Combat</p>
               <p>Michel Bay est un aventurier ayant fait fortune grâce à ses nombreux livres racontant ses périples remplis d'explosions et d'action. Il décide de construire une gigantesque tour en plein désert afin de trouver la guidoune la plus valeureuse du monde, au fil d'une impitoyable série de combats.</p>
-              <p>Il faisait partie d'un trio de voyageurs aux aventures bourrées de péripétie, mais chacun a suivi une voie différente.</p>
+              <p>Il faisait partie d'un quatuor de voyageurs aux aventures bourrées de péripétie, mais chacun a suivi une voie différente.</p>
+              <br />
+
+              <h3 className="subtitle">P'tit Billy</h3>
+              <p><u>Fonction :</u> Aucune, c'est juste un gamin</p>
+              <p><u>Lieu(x) de rencontre :</u> Nouillorc, Nowel City, Sainte-Moustache-le-Bosquet</p>
+              <p>P'tit Billy est un gamin âgé de 8 ans, vivant avec ses "parents" à Nouillorc. Il rencontre plusieurs fois les Chosen Ones, toujours heureux de l'aider à résoudre ses problèmes quotidiens, sources d'aventures remplies d'amitié et de bons sentiments. Il considère d'ailleurs les Chosen Ones comme ses meilleurs amis, au grand bonheur de ses "parents".</p>
+              <p>La présence d'enfants dans l'aventure est toujours un sujet épineux pour Guillaume.</p>
               <br />
 
               <h3 className="subtitle">Hubert Reeves</h3>
@@ -62,11 +69,25 @@ export class Ami extends React.Component {
               <p>Il mentionne son frère ayant suivi le chemin de la piraterie, vu pour la dernière fois aux alentours de Takarajima il y'a des années.</p>
               <br />
 
+              <h3 className="subtitle">Sylvestre Talon</h3>
+              <p><u>Fonction :</u> Directeur de la Pyramide de Combat</p>
+              <p><u>Lieu(x) de rencontre :</u> Pyramide de Combat</p>
+              <p>Sylvestre est un boxeur vétéran de guerre ayant fait fortune grâce à ses nombreux livres racontant ses périples remplis de muscles et d'action. Il réaménage à lui seul une pyramide à l'abandon afin d'y installer un labyrinthe magique changeant d'aspect à chaque passage, afin de trouver la guidoune la plus maligne du monde.</p>
+              <p>Il faisait partie d'un quatuor de voyageurs aux aventures bourrées de péripétie, mais chacun a suivi une voie différente.</p>
+              <br />
+
               <h3 className="subtitle">Chef Jibé</h3>
               <p><u>Fonction :</u> BG de la Vie</p>
               <p><u>Lieu(x) de rencontre :</u> Port Duavre, Îles lointaines</p>
               <p>Le vétéran des 3 BG de la Vie. Chef Jibé rencontre les Chosen Ones au port Duavre, après que les Jhâvas aient scellé l'accès à la route du grand chemin. Il dispose d'une grande connaissance des différents mondes, bien qu'il ne soit qu'n simple observateur.</p>
               <p>En dépit de son statut, il est en mesure d'aider les Chosen Ones à progresser à travers les redoutables Îles lointaines.</p>
+              <br />
+
+              <h3 className="subtitle">Jean-Paul</h3>
+              <p><u>Fonction :</u> Bonne question</p>
+              <p><u>Lieu(x) de rencontre :</u> CCI, Îles lointaines</p>
+              <p>Jean-Paul est un individu étrange en étroite relation avec les Divinités Gilles et Guillaume, doté d'un rôle très obscur. Il est en mesure de forger des armes fabuleuses, destinée à chaque Chosen One moyennant des matériaux extrêmement rares.</p>
+              <p>Jean-Paul semble être mentionné dans les livres parlant de la Création, signifiant qu'il serait au moins aussi vieux que les Divinités elles-mêmes.</p>
               <br />
               </div>
         </div>
