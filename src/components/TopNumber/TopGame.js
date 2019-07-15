cd333395fa80c63e19fd014b3f7761f530c8cb06
Adrien L <a.lepinay@attineos.com>
2019-07-15 15:37:29 +0200

    Fix chemins

--- src/components/TopNumber/TopGame.js
+++ src/components/TopNumber/TopGame.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../index.css';
+import '../../index.css';
 //import * as serviceWorker from '../serviceWorker';
 import { TopNumber } from './TopNumber';
 import { Display } from './Display';
